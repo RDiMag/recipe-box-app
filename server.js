32aff6fb119df87a603515cc24ee43e742e0d66e
RDiMag <87457132+RDiMag@users.noreply.github.com>
2023-02-12 20:16:27 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from "express"
+import axios from "axios"
+import cors from "cors"
+import "dotenv/config"
+import { MongoClient } from "mongodb"
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+
+app.get('/recipes/:query', async (req, res) => {
+  const response = await axios.get(
+    `https://api.edamam.com/api/recipes/v2?type=public&q=${req.params.query}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`
+  )
+  console.log(response.data.hits)
+  res.json(response.data.hits)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is listening to port ${PORT}`)
+})
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'recipebox'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
---
+++ src/recipe.js
@@ -0,0 +1,19 @@
+import React from 'react'
+// import './recipestyle.css'
+import { Card } from 'react-bootstrap';
+
+const Recipe = ({ title, image, source, url }) => {
+  return(
+    <Card border="dark" style={{ width: ''}} className='recipe'>
+      <Card.Img variant="top" src={image} alt="recipe" />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+            <Card.Text>
+              <a href={url}>{source}</a>
+            </Card.Text>
+        </Card.Body>
+    </Card>
+  )
+}
+
+export default Recipe
