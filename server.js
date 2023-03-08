5a99ff94b0e5a76a71573c3046830faf36bcb727
RDiMag <87457132+RDiMag@users.noreply.github.com>
2023-03-08 11:29:52 -0500

    updating scripts

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(cors())
-app.use(express.static('public'))
+app.use(express.static(process.env.PUBLIC_DIR))
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 
